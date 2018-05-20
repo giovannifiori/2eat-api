1a802da08a60aa25e2953b1091ec6a78e2bf087d
Giovanni Fiori <giovannicfiori@gmail.com>
2018-05-20 10:56:39 -0400

    update user auth rules

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,14 +5,13 @@ import checkAuth from '../middleware/auth';
 const user = new UserController();
 const router = express.Router();
 
-router.use(checkAuth);
-router.route('/').get(user.getAll).post(user.create);
-router.route('/search/:name').get(user.find);
+router.route('/').get(checkAuth, user.getAll).post(user.create);
+router.route('/search/:name').get(checkAuth, user.find);
 router.route('/auth').post(user.authorize);
-router.route('/user/:id').get(user.getById).patch(user.update).delete(user.delete);
-router.route('/user/:id/follow/:followId').post(user.follow);
-router.route('/user/:id/unfollow/:unfollowId').post(user.unfollow);
-router.route('/user/:id/following').get(user.getFollowing);
-router.route('/user/:id/followers').get(user.getFollowers);
+router.route('/user/:id').get(checkAuth, user.getById).patch(checkAuth, user.update).delete(checkAuth, user.delete);
+router.route('/user/:id/follow/:followId').post(checkAuth, user.follow);
+router.route('/user/:id/unfollow/:unfollowId').post(checkAuth, user.unfollow);
+router.route('/user/:id/following').get(checkAuth, user.getFollowing);
+router.route('/user/:id/followers').get(checkAuth, user.getFollowers);
 
 export { router as userRouter };
