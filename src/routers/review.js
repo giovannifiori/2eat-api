d57cec7ac62279cb52a43a1e853b614beb543247
Giovanni Fiori <giovannicfiori@gmail.com>
2018-05-19 20:14:06 -0400

    add auth check on review

--- src/routers/review.js
+++ src/routers/review.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import ReviewController from '../controllers/review';
+import checkAuth from '../middleware/auth';
 
 const router = express.Router();
 const review = new ReviewController();
+
+router.use(checkAuth);
 
 router.route('/').post(review.create);
 router.route('/review/:id').get(review.getById).delete(review.delete);
